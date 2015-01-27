c9e4e295e9fc65a5ef304a45f33300f53139e588
nishizhen <nishizhen-fly@163.com>
2015-01-27 23:19:21 +0800

    add basehoteltest

---
+++ src/com/eric/homework/test/BaseHotelTest.java
@@ -0,0 +1,64 @@
+package com.eric.homework.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class BaseHotelTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void testBaseHotel() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testBaseHotelString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCalculateDayCharge() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCalculateTotalCharge() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testIsVIP() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDefineDiscount() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCaculateTotalPrice() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDefinePrice() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetPriceMap() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetPriceMap() {
+		fail("Not yet implemented");
+	}
+
+}
